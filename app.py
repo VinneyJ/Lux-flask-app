a7bc7ccb0200875e42ec857e7f11df04b56912d3
Vince <vincentjayden49@gmail.com>
2021-09-17 01:14:51 +0300

    create posts and db

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
-from flask import Flask
-
+from abc import abstractproperty
+from flask import Flask, request, render_template
+from flask_mysqldb import MySQL
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+msql = MySQL(app)
+
+#config
+
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
+
+@app.route("/", methods=['GET', 'POST'])
+def post_article():
+    
+    if request.method == 'POST':
+        postsObj = request.form
+        title = postsObj["title"]
+        content = postsObj["content"]
+        
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True )
